ffd60d4aafd3c60ec2fd6d0d9e8f9ee8724b59fb
LucreR2003 <Lucrerios95@gmail.com>
2022-04-24 18:04:24 -0300

    agrego App.js y creo estructura de Html

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+
+const path = require("path")
+
+const publicPath = path.resolve(__dirname, "./public")
+app.use(express.static(publicPath))
+
+const port = 3000
+app.listen(port, () => console.log("Server up and running on the port " + port))
+
+app.get("/", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+app.get("/productDescription", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/productDescription.html"))
+})
+app.get("/buyCart", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/buyCart.html"))
+})
+app.get("/login", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+app.get("/registerFormulary", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/registerFormulary.html"))
+})
+app.get("*", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/notFound.html"))
+})
