bd7476e2b1c82abdd534f3bace7fb88be5db62f8
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-06-29 17:48:30 -0300

    Agrego validacion que controle si la contraseña de confirmacion es igual a la contraseña

--- src/middlewares/registerMiddleware.js
+++ src/middlewares/registerMiddleware.js
@@ -4,8 +4,15 @@ const validateRegister = [
     body("userEmail").notEmpty().withMessage("Debe tener un email").bail()
         .isEmail().withMessage("Debe tener formato de email"),
     body("userName").notEmpty().withMessage("Debes escribir un nombre"),
-    body("userPassword").notEmpty().withMessage("Debes escribir una contraseña"),
-    body("passwordConfirmation").notEmpty().withMessage("Debes escribir una contraseña de confirmación")
+    body("userPassword").notEmpty().withMessage("Debes escribir una contraseña").bail()
+        .isLength({ min: 8 }).withMessage("La contraseña debe tener más de 7 caracteres"),
+    body("passwordConfirmation").notEmpty().withMessage("Debes escribir una contraseña de confirmación").bail()
+        .custom((password, { req }) => {
+            if(password != req.body.userPassword) {
+                throw new Error('Las contraseñas no coinciden');
+            }
+            return true;
+        })
 ];
 
 module.exports = validateRegister;
