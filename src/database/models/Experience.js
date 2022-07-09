66bb053b1c64c078a4761e48a3d6cd4888a55150
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-07-09 11:33:02 -0300

    Modelo de la experiencia

---
+++ src/database/models/Experience.js
@@ -0,0 +1,66 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const alias = 'Experience';
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING(500),
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        people_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        duration_type: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        currency: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL(9,2),
+            allowNull: false
+        },
+        map: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        user: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }
+
+    const config = {
+        timestamps: false,
+        underscore: true
+    }
+    
+    const Experience = sequelize.define(alias, cols, config);
+
+    return Experience;
+}
