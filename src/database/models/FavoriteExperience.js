6e4455431789f6470ef0ae8e43ef89d7ad30f8ae
julianblazquez18 <juliblazquez2004@gmail.com>
2022-07-09 21:45:58 -0300

    Agregando model de FavoriteExperience

---
+++ src/database/models/FavoriteExperience.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const alias = 'FavoriteExperience';
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        experience: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }
+
+    const config = {
+        timestamps: false,
+        underscore: true
+    }
+
+    const FavoriteExperience = sequelize.define(alias, cols, config);
+   
+    FavoriteExperience.associate = function(models){
+
+        FavoriteExperience.belongsTo(models.Experience, {
+            as: 'experience',
+            foreignKey: 'experience'
+        })
+        FavoriteExperience.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: 'user'
+        })
+    }
+
+}
---
+++ src/database/models/User.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'User';
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        first_name: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        last_name: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        password: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        image: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        bio: {
+            type: dataTypes.STRING(500),
+        },
+    };
+    let config = {
+        timestamps: false,
+    }
+    
+    const User = sequelize.define(alias, cols, config); 
+
+    User.associate = function(models){
+
+        User.hasMany(models.Experience, {
+            as: "experiences",
+            foreignKey: "user", 
+        })
+        
+        User.hasMany(models.FavoriteExperience, {
+            as: "favoritesExperiences",
+            foreignKey: "user",
+        })
+    }
+
+    return User
+};
