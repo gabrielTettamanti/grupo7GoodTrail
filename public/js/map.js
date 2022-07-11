d1c0f99cab25b33e5fbed82921eeb2c2fa935a21
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-07-11 18:15:19 -0300

    Mapa de Google Maps, API y logica

--- public/js/map.js
+++ public/js/map.js
@@ -2,15 +2,16 @@
 const initMap = () => {
 
     //***** Map creation *****/
-    const map = new google.maps.Map(document.querySelector('.map'), { zoom: 10 });
+    const map = new google.maps.Map(document.querySelector('.map'), { zoom: 15 });
 
     //***** Geocoder initialization *****/
     const geocoder = new google.maps.Geocoder();
 
-    const address = document.querySelector('.address');
+    const address = document.querySelector('.address').innerHTML;
 
     geocoder.geocode({ address: address}, (results, status) => {
         if(status == 'OK') {
+            console.log(results);
             map.setCenter(results[0].geometry.location);
             const marker = new google.maps.Marker({
                 map: map,
