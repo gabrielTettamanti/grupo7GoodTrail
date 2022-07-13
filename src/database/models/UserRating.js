47d2188b82b7d3286261a07cba87e76d9cefa7e3
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-07-13 17:03:14 -0300

    Creacion de modelo UserRating

---
+++ src/database/models/UserRating.js
@@ -0,0 +1,57 @@
+const User = require("./User");
+
+module.exports = (sequelize, DataTypes) => {
+
+    const alias = "UserRating";
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        vote: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        creation_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        rating_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'rating',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        }
+    }
+
+    let config = {
+        timestamps: false,
+    }
+
+    const UserRating = sequelize.define(alias, cols, config); 
+
+    UserRating.belongsTo(User, 
+       {
+        foreignKey: "user_id"
+       });
+       
+    UserRating.belongsTo(rating, 
+       {
+        foreignKey: "rating_id"
+       });
+
+    return UserRating
+}
