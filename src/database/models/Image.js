f2001e0e4280119a1ca202c1594ab9b40c05e1a0
LucreR2003 <lucrerios95@gmail.com>
2022-07-15 11:48:07 -0300

    Agrego modelo imagen

---
+++ src/database/models/Image.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = "Image"
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        experience_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true
+        },
+    }
+
+const config = {
+    timestamps: false,
+    underscore: true
+}
+
+const Image = sequelize.define(alias, cols, config);
+
+ Image.associate = models => {   
+    Image.belongsTo(models.Experience, {
+    as: 'experience',
+    foreignKey: 'experience'
+});
+}
+return Image;
+}
