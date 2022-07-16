6eafa4941c5664b5a95236687ca99c2c7e1eadbf
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-07-16 18:32:15 -0300

    Configuracion de Nodemailer

--- src/config.js
+++ src/config.js
@@ -4,5 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    mapsKey: process.env.GOOGLE_MAPS_KEY
+    mapsKey: process.env.GOOGLE_MAPS_KEY,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    refreshToken: process.env.REFRESH_TOKEN
 }
---
+++ src/services/mailer.service.js
@@ -0,0 +1,47 @@
+//***** Require´s *****/
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const { clientId, clientSecret, redirectUri, refreshToken } = require('../config');  
+
+const Mailer = {
+    sendEmail: async (email, randomPassword) => {
+        //***** oAuth2 Configuration *****/
+        const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
+
+        oAuth2Client.setCredentials({ refresh_token: refreshToken });
+
+        const accessToken = oAuth2Client.getAccessToken();
+
+        //***** Email Body generation *****/
+        const contentHTML = `
+            <h1>Contraseña generada</h1>
+            <p>Tu nueva contraseña es: ${randomPassword} <strong> </strong></p> 
+        `;
+
+        //***** Nodemailer Configuration *****/
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAUTH2',
+                user: 'goodtrailexperiences@gmail.com',
+                clientId: clientId,
+                clientSecret: clientSecret,
+                refreshToken: refreshToken,
+                accessToken: accessToken
+            }
+        });
+
+        const mailOptions = {
+            from: 'GoodTrail Experiences <goodtrailexperiences@gmail.com>',
+            to: email,
+            subject: 'Nueva contraseña generada',
+            html: contentHTML
+        };
+
+        const result = await transporter.sendMail(mailOptions);
+
+        return result;
+    }
+}
+
+module.exports = Mailer;
