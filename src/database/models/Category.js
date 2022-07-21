f59d8921f5de6c22ec88627fe07b23069a1525e7
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-07-21 11:40:34 -0300

    Creando armado de modelo Category

---
+++ src/database/models/Category.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+    
+    const alias = "Category"
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        category_name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        style: {
+            type: DataTypes.STRING(100),
+        }
+    }
+
+    const config = {
+        timestamps: false,
+        underscore: true
+    }
+
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = models => {   
+
+        Category.hasMany(models.Experience, {
+            as: "experiences",
+            foreignKey: "category_id", 
+        })
+
+    }
+    
+    return Category;
+}
