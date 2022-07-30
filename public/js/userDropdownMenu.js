e6e1a2751fd487e4f097c2d1c26571745e798ab4
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-07-29 22:08:51 -0300

    Estilos del dropdown-menu del usuario

--- public/js/userDropdownMenu.js
+++ public/js/userDropdownMenu.js
@@ -2,9 +2,11 @@ window.addEventListener('load', () => {
     const dropdown = document.querySelector('.dropdown');
     const userSelect = dropdown.querySelector('.user-select');
     const userMenu = dropdown.querySelector('.user-menu');
+    const arrow = dropdown.querySelector('.arrow');
 
     userSelect.addEventListener('click', () => {
         console.log('Hiciste click');
         userMenu.classList.toggle('menu-open');
+        arrow.classList.toggle('arrow-rotate');
     });
 });
