857f7bb9b9860936614d753cebb24ae565f58b4f
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-08-02 15:25:49 -0300

    Validaciones contraseña backend

--- src/middlewares/registerMiddleware.js
+++ src/middlewares/registerMiddleware.js
@@ -5,7 +5,7 @@ const validateRegister = [
         .isEmail().withMessage("Debe tener formato de email"),
     body("userName").notEmpty().withMessage("Debes escribir un nombre").isLength({ min: 2 }).withMessage("Debe tener por lo menos 2 caracteres."),
     body("userPassword").notEmpty().withMessage("Debes escribir una contraseña").bail()
-        .isLength({ min: 8 }).withMessage("La contraseña debe tener más de 7 caracteres"),
+        .isLength({ min: 8 }).withMessage("La contraseña debe tener más de 8 caracteres"),
     body("passwordConfirmation").notEmpty().withMessage("Debes escribir una contraseña de confirmación").bail()
         .custom((password, { req }) => {
             if(password != req.body.userPassword) {
