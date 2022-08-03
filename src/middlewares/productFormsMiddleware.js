2a3d993349f5e29824d3f71dd0fd6b4c99b56990
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-02 21:11:21 -0300

    Validacion name y description experiencias en el back

--- src/middlewares/productFormsMiddleware.js
+++ src/middlewares/productFormsMiddleware.js
@@ -3,9 +3,9 @@ const { body } = require('express-validator');
 
 const validateForm = [
     body('name').notEmpty().withMessage('La experiencia debe tener un nombre').bail()
-        .isLength({ min: 4 }).withMessage('El nombre de la experiencia debe contener más de 3 caracteres'),
+        .isLength({ min: 5 }).withMessage('El nombre de la experiencia debe contener al menos 5 caracteres'),
     body('description').notEmpty().withMessage('La experiencia debe tener una descripción').bail()
-        .isLength({ min: 10 }).withMessage('La descripción de la experiencia debe tener más de 9 caracteres'),
+        .isLength({ min: 20 }).withMessage('La descripción de la experiencia debe tener al menos 20 caracteres'),
     body('location').notEmpty().withMessage('La experiencia debe tener un destino').bail(),
     body('people_quantity').notEmpty().withMessage('La experiencia debe tener la cantidad de personas').bail()
         .isInt().withMessage('La cantidad de personas debe ser un número'),
