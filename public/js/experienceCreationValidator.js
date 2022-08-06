5123aa370858a2a564bfa6470274fe336850ca55
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-06 14:03:27 -0300

    Validaciones front de la creacion de experiencia

---
+++ public/js/experienceCreationValidator.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector('.forms');
+    const formElements = document.querySelectorAll('.formElement');
+
+    form.addEventListener('submit', e => {
+        let errors = [];
+
+        formElements.forEach(input => {
+            if(input.value === ''){
+                errors.push(input);
+            }
+        });
+
+        if(errors.length > 0){
+            e.preventDefault();
+        }
+    });
+
+    formElements.forEach(input => input.addEventListener('blur', e => {
+        const element = e.srcElement;
+        const p = document.querySelector('#error' + element.id);
+        if(element.value === ''){
+            element.classList.add('invalid');
+            p.innerHTML = `El campo ${element.id} no puede estar vacio`;
+        } else if(element.name === 'name' && element.value.length < 5) {
+            p.innerHTML = `El campo ${element.id} debe tener más de 5 caracteres`;
+        } else if(element.name === 'description' && element.value.length < 20){
+            p.innerHTML = `El campo ${element.id} debe tener más de 20 caracteres`;
+        } else {
+            p.innerHTML = "";
+        }
+    }));
+});
