32ab1579379512840f241963c6c3612aa79866cc
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-06 18:03:35 -0300

    Estilos de los input al validar el front de creacion de exp

--- public/js/experienceCreationValidator.js
+++ public/js/experienceCreationValidator.js
@@ -20,14 +20,24 @@ window.addEventListener('load', () => {
         const element = e.srcElement;
         const p = document.querySelector('#error' + element.id);
         if(element.value === ''){
-            element.classList.add('invalid');
+            manageStyle(element);
             p.innerHTML = `El campo ${element.id} no puede estar vacio`;
         } else if(element.name === 'name' && element.value.length < 5) {
+            manageStyle(element);
             p.innerHTML = `El campo ${element.id} debe tener más de 5 caracteres`;
         } else if(element.name === 'description' && element.value.length < 20){
+            manageStyle(element);
             p.innerHTML = `El campo ${element.id} debe tener más de 20 caracteres`;
         } else {
+            element.classList.remove('invalid');
+            element.classList.add('valid');
             p.innerHTML = "";
         }
     }));
+
+    const manageStyle = element => {
+        element.classList.remove('normal-border');
+        element.classList.add('invalid');
+        element.classList.remove('valid');
+    }
 });
