14a5d5629ee9f088b04f68f4338fb35bf2ec39bb
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-08-13 16:42:17 -0300

    Empece el servicio de listado de usuarios en la api

--- src/api/services/user.api.js
+++ src/api/services/user.api.js
@@ -1,7 +1,26 @@
 ///// Requires //////
 const UserService = require('../../../services/user.service');
 
+
 const UserAPI = {
+    usersList: (req,res) => {
+        UserService.getUserList()
+        .then(users => {
+            return res.status(200).json({
+                meta: {
+                    status: 200
+                },
+                data: {
+                }
+            });
+        })
+        .catch(error => {
+            return res.status(400).json({
+                status: 400,
+                message: error.message
+            })
+        });
+    }
 }
 
 module.exports = UserAPI;
