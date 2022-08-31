308b7431636192dad7b5e29b0cdf6d76726a6938
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-08-30 21:45:08 -0300

    Añado algunas exp a la base de datos

--- src/data/insert.sql
+++ src/data/insert.sql
@@ -2,7 +2,7 @@
 -- Users insert
 -- -----------------------------------------------------
 INSERT INTO `user`(id, first_name, last_name, email, `password`, image, bio)
-VALUES (default, "Ignacio", "Lopez", "ignaciolopestela12@gmail.com", "$2a$10$OPkwQVeeDzuxLythxBfNvur7DkwApqgVaBTTaxvFr4CkKMMtkw6z2", "1654440409516-avatar-test.jpeg", "Me dicen Nacho. Soy uno de los creadores de la página. Aguante Boca!"),(default, "Gabriel", "Tettamanti", "gabrieltettamanti.dev@gmail.com", "$2a$10$K03891dZx9vohC1biqhCP.OvFJ.nrn8bKkUtZQJIR4hmbwxFAZ2sO", "profile-default.jpg", ""), (default, "Julian", "Julian", "julian@gmail.com", "$2a$10$.mShdg4qCVmimsFBi.snoOribx0T0n772QYK6xGP8n7LowTBzNYGC", "1655250027506-938446.jpg", ""),(default, "lucre", "lucre", "lucrerios95@gmail.com", "$2a$10$gYl9kN7ckhF86h/6CVHhtODgSx8FGg2G2kkE0cOnAO4i6f8HTQpWG", "profile-default.jpg", "");
+VALUES (default, "Ignacio", "Lopez", "ignaciolopestela12@gmail.com", "$2a$10$OPkwQVeeDzuxLythxBfNvur7DkwApqgVaBTTaxvFr4CkKMMtkw6z2", "1654440409516-avatar-test.jpeg", "Me dicen Nacho. Soy uno de los creadores de la página. Aguante Boca!"),(default, "Gabriel", "Tettamanti", "gabrieltettamanti.dev@gmail.com", "$2a$10$K03891dZx9vohC1biqhCP.OvFJ.nrn8bKkUtZQJIR4hmbwxFAZ2sO", "profile-default.jpg", ""), (default, "Julian", "Blazquez", "julian@gmail.com", "$2a$10$.mShdg4qCVmimsFBi.snoOribx0T0n772QYK6xGP8n7LowTBzNYGC", "1655250027506-938446.jpg", ""),(default, "Lucre", "Rios", "lucrerios95@gmail.com", "$2a$10$gYl9kN7ckhF86h/6CVHhtODgSx8FGg2G2kkE0cOnAO4i6f8HTQpWG", "profile-default.jpg", "");
 
 -- -----------------------------------------------------
 -- Category insert
@@ -23,25 +23,27 @@ Values (1, "Santiago Bernabeu", 1, "Visita el icónico estadio de Madrid y viví
 (7, "Hotel Madero", 1, "Una noche para 2 personas en el prestigioso hotel de Puerto Madero.", "CABA - Buenos Aires", 2, 1, "día", "$", 65000, "Hotel Madero Buenos Aires, Argentina", 2, 4),
 (8, "Rabieta", 1, "Cena para 4 personas en el bar ubicado en el hipodromo de Palermo.", "CABA - Buenos Aires", 4, 0, "día", "$", 8000, "Rabieta Avenida Libertador, Buenos Aires", 2, 3),
 (9, "Golden State Warriors", 1, "Noche de NBA en la casa del gran Stephen Curry.", "San Francisco - USA", 2, 2, "días", "$", 500000, "Chase Center San Francisco, California", 2, 7),
-(10, "Santa Monica Burguer House", 1, "Viaja a la ciudad de Quilmes para comer las mejores hamburgesas del país (y posiblemente del mundo). Unica hamburguseria que tiene un gato como mascota llamado 'Santos'.", "Quilmes - Buenos Aires", 4, 3, "horas", "$", 8000, "Santa Monica Burguers Quilmes, Buenos Aires", 1, 3);
+(10, "Santa Monica Burguer House", 1, "Viaja a la ciudad de Quilmes para comer las mejores hamburgesas del país (y posiblemente del mundo). Unica hamburguseria que tiene un gato como mascota llamado 'Santos'.", "Quilmes - Buenos Aires", 4, 3, "horas", "$", 8000, "Santa Monica Burguers Quilmes, Buenos Aires", 1, 3),
+(11, "Museo de La Plata", 1, "Es una de las instituciones más emblemáticas de la ciudad capital de la provincia de Buenos Aires. Pertenece a la Facultad de Ciencias Naturales y Museo de la Universidad Nacional de La Plata.", "La Plata - Buenos Aires", 4, 6, "horas", "$", 2000, "Museo de La Plata, La Plata, Buenos Aires Province", 2, 6),
+(12, "Malba", 1, "El Malba combina un calendario de exposiciones temporales, con la exhibición estable de su colección institucional, y funciona simultáneamente como un espacio plural de producción de actividades culturales y educativas.", "Buenos Aires", 3, 6, "horas", "$", 1000, "Museo de Arte Latinoamericano, Avenida Presidente Figueroa Alcorta, Buenos Aires", 2, 6);
 
 -- -----------------------------------------------------
 -- Rating insert
 -- -----------------------------------------------------
 INSERT INTO rating (id, value, experience_id) 
-VALUES (default, 5, 1), (default, 4, 2), (default, 5, 3), (default, 5, 4), (default, 4, 5), (default, 3, 6), (default, 4, 7), (default, 4, 8), (default, 5, 9), (default, 5, 10); 
+VALUES (default, 5, 1), (default, 4, 2), (default, 5, 3), (default, 5, 4), (default, 4, 5), (default, 3, 6), (default, 4, 7), (default, 4, 8), (default, 5, 9), (default, 5, 10),(default, 5, 11),(default, 5, 12); 
 
 -- -----------------------------------------------------
 -- Experiences images insert
 -- -----------------------------------------------------
 INSERT INTO experience_image (id, url, experience_id) 
-VALUES (1, 'bernabeuCatalog.jpg', 1), (2, 'gizaCatalog.png', 2), (3, 'romaCatalog.jpg', 3), (4, 'liverpool.jpg' , 4), (5, 'stampa.jpg' ,5), (6, 'temple.jpg' ,6), (7, 'hotelMadero.avif' ,7), (8, 'rabieta.jpg' ,8), (9, 'warriors.jpg',9), (10, 'california.png', 10);
+VALUES (1, 'bernabeuCatalog.jpg', 1), (2, 'gizaCatalog.png', 2), (3, 'romaCatalog.jpg', 3), (4, 'liverpool.jpg' , 4), (5, 'stampa.jpg' ,5), (6, 'temple.jpg' ,6), (7, 'hotelMadero.avif' ,7), (8, 'rabieta.jpg' ,8), (9, 'warriors.jpg',9), (10, 'california.png', 10), (11, 'museoPlata.jpg', 11), (12, 'malba.jpg', 12);
 
 -- -----------------------------------------------------
 -- Offer insert
 -- -----------------------------------------------------
 INSERT INTO Offer (id, status, discount, limit_date, experience_id)
-VALUES (1, 0, null, null, 1),(2, 0, null, null, 2),(3, 0, null, null, 3),(4, 0, null, null, 4),(5, 0, null, null, 5),(6, 0, null, null, 6),(7, 0, null, null, 7),(8, 0, null, null, 8),(9, 0, null, null, 9);
+VALUES (1, 0, null, null, 1),(2, 0, null, null, 2),(3, 0, null, null, 3),(4, 0, null, null, 4),(5, 0, null, null, 5),(6, 0, null, null, 6),(7, 0, null, null, 7),(8, 0, null, null, 8),(9, 0, null, null, 9),(10, 0, null, null, 10), (11, 0, null, null, 11), (12, 0, null, null, 12);
 
 -- -----------------------------------------------------
 -- CartExperience insert
